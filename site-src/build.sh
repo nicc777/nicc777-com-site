24947ea6c709dfd7a327eda7b881bd152f409f68
Nico Coetzee <nicc777@gmail.com>
2022-02-24 06:17:23 +0100

    Updated build script

--- site-src/build.sh
+++ site-src/build.sh
@@ -2,4 +2,16 @@
 
 mkdocs build
 
-cp -vf site/404.html site/error.html
+#cp -vf site/404.html site/error.html
+
+cat << EOF > site/error.html
+<html>
+<head>
+<meta http-equiv="refresh" content="2;url=http://www.nicc777.com/" />
+<title>Page Moved</title>
+</head>
+<body>
+This page has moved. Click <a href="http://www.nicc777.com/">here</a> to go to the new page.
+</body>
+</html>
+EOF
